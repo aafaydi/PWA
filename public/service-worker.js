976704120a24b104dd42cbf390db5b7a3e0a9bab
Ahmad Faydi <aafaydi@hotmail.com>
2022-06-12 21:32:32 -0400

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/css/styles.css",
     "/js/index.js",
-    "/js/db.js",
+    "/js/idb.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
